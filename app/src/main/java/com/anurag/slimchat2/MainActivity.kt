e43dd31d3592d921517c8a212f1ad78616b8af36
Anurag Kumar Singh <singh.ask@outlook.com>
2023-10-08 00:00:21 +0530

    Updated Main to accomodate New Ui!

--- app/src/main/java/com/anurag/slimchat2/MainActivity.kt
+++ app/src/main/java/com/anurag/slimchat2/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   Main()
                 }
             }
         }
@@ -30,17 +29,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Main() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Slimchat2Theme {
-        Greeting("Android")
+      Main()
     }
 }
